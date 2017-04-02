25519f1ebe98299daa131875d9c3752fbbb13bd9
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-04-02 22:24:12 +0800

    Added backtracking algorithm. Tested n = 1 to 15.

--- NQueenProblem/Program.cs
+++ NQueenProblem/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace NQueenProblem
 {
@@ -10,6 +7,69 @@ namespace NQueenProblem
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 15; i++)
+                QueenProblem(i);
+        }
+ 
+        static void QueenProblem(int n){
+            var solutionCount = new long[1];
+            var stopWatch = new Stopwatch();
+ 
+            stopWatch.Start();
+            QueenProblem(0, new int[n], solutionCount);
+            stopWatch.Stop();
+ 
+            Console.WriteLine($"Number of solution for {n} x {n} is {solutionCount[0]}. Used time = {stopWatch.Elapsed.TotalSeconds: 0.00} seconds.");
+        }
+ 
+        static void QueenProblem(int currentRow, int[] solution, long[] solutionCount)
+        {
+            var dimension = solution.Length;
+ 
+            if (currentRow == dimension) {
+                solutionCount[0]++;
+                //Console.WriteLine(string.Join(", ", solution));
+                return;
+            }
+ 
+            bool verticalTest, diagonalTest;
+            int step, row, diff, value1, value2;
+ 
+            for (int position = 0; position < dimension; position++) {
+                verticalTest = true;
+                diagonalTest = true;
+ 
+                //Check vertical
+                for (step = 0; step <= currentRow - 1; step++)
+                    if (solution[step] == position) {
+                        verticalTest = false;
+                        break;
+                    }
+                if (!verticalTest) continue;
+ 
+                //Check diagonal
+                row = currentRow - 1;
+                diff = currentRow - row;
+                while (row >= 0) {
+                    value1 = position - diff;
+                    value2 = position + diff;
+                    step = currentRow - diff;
+ 
+                    if (solution[step] == value1 || solution[step] == value2) {
+                        diagonalTest = false;
+                        break;
+                    }
+                    diff++;
+                    row--;
+                }
+ 
+                //If both tests passed, go to next level
+                if(verticalTest && diagonalTest) {
+                    solution[currentRow] = position;
+                    QueenProblem(currentRow + 1, solution, solutionCount);
+                }
+            
+            }
         }
     }
 }
